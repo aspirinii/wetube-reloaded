56394612adf8b5e7894e0356bd179c2e42e5b1dd
aspirinii <aspiriniijk@gmail.com>
2024-02-10 21:31:45 +0900

    change port

--- src/init.js
+++ src/init.js
@@ -6,10 +6,11 @@ import "./models/User";
 import "./models/Comment";
 import app from "./server";
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 const handleListening = () =>
-    console.log(`✅ Listening on: http://localhost:${PORT} 🚀`);
+    // console.log(`✅ Listening on: http://localhost:${PORT} 🚀`);
+    console.log(`✅ Listening on: https://kpopmv.onrender.com/:${PORT} 🚀`);
 
 app.listen(PORT, handleListening);
 //app.listen: 이 함수는 지정된 포트에서 네트워크 요청을 수신할 준비를 합니다. 이 때, app.set과 app.use를 통해 설정된 모든 사항들이 적용된 상태로 서버가 시작됩니다.
